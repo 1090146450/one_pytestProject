fc4065f742eae0f4e8a14c446c1d1a4c5e119e46
Administrator <2933310375@qq.com>
2023-03-16 13:52:18 +0800

    添加发送邮件功能

--- Commom/Email.py
+++ Commom/Email.py
@@ -5,16 +5,16 @@ from Params import tools
 
 class send_email:
     def emil(self):
-        email_yam = tools.YamlUtil("", file="./Config/passwd.yaml").read_yaml()
+        email_yam = tools.YamlUtil("", file="../Config/passwd.yaml").read_yaml()
         # 建立对象连接
         yag = yagmail.SMTP(user={email_yam[0]["email_account"]: "测试服务器"}, password=email_yam[0]["email_passwd"],
                            host="smtp.qq.com")
         # 收件人
-        to = ["1090146450@qq.com"]
+        to = ["1090146450@qq.com", "293310375@qq.com"]
         # 邮件主题
-        title = "测试邮件"
+        title = "Jenkins警告！"
         # 邮件正文
-        cont = "这是一个测试的邮件，请勿回复"
+        cont = "Jenkins又挂了，，诶"
         # 附件列表
         atta = "这里填写路径"
         # 抄送联系人
@@ -23,3 +23,6 @@ class send_email:
         yag.send(to=to, subject=title, contents=cont, cc=cc)
         # 关闭邮件
         yag.close()
+
+
+send_email().emil()
